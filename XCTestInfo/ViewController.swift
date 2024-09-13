b38aac194d87a9f0f37e696e5300c2bcb9aee0a6
sungl <sungl@88.com>
2024-09-13 14:41:19 +0800

    创建项目

---
+++ XCTestInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCTestInfo
+//
+//  Created by 孙国立 on 2024/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
